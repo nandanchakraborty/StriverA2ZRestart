3973d640b11c8498bd327e1cc8ac147985dbe9c5
nandanchakraborty <nandancsebubt@gmail.com>
2025-11-08 11:30:21 +0600

    Return the element that occure only ones in an array

--- src/SolvingProblemOnArray/ReturnTheElementThatOccureOnes.java
+++ src/SolvingProblemOnArray/ReturnTheElementThatOccureOnes.java
@@ -1,4 +1,40 @@
 package SolvingProblemOnArray;
 
+import java.util.Scanner;
+
+/**
+Leetcode
+ * Hint
+ * Given a non-empty array of integers nums, every element appears twice except for one. Find that single one.
+ * You must implement a solution with a linear runtime complexity and use only constant extra space.
+
+ * Example 1:
+
+ * Input: nums = [2,2,1]
+
+ * Output: 1
+ */
 public class ReturnTheElementThatOccureOnes {
+    public static int singleNumber(int[] nums) {
+        int value =0;
+        for(int i =0;i<nums.length;i++){
+            value = value ^ nums[i];
+
+        }
+        return value;
+
+    }
+    public static void main(String[]args){
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int[] ar1 = new int[n];
+        for (int i = 0; i < n; i++) {
+            int b = sc.nextInt();
+            ar1[i] = b;
+        }
+        int singleValue = singleNumber(ar1);
+        System.out.println("value that appear one "+singleValue);
+
+
+    }
 }
